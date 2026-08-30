abba78ee66a424b2bbd46172b564c39e140a7b9e
؟MARTO <programmers.atlantis@gmail.com>
2026-08-31 03:09:02 +0330

    Update exceptions.py

--- hidmart/exceptions.py
+++ hidmart/exceptions.py
@@ -1,23 +1,18 @@
 class HidMartError(Exception):
     """Base exception for HidMart."""
-
-
-class ConnectionError(HidMartError):
-    """Raised when the connection to Bale fails."""
-
-
-class AuthenticationError(HidMartError):
-    """Raised when authentication fails."""
+    pass
 
 
 class APIError(HidMartError):
-    """Raised when Bale returns an API error."""
-
-    def __init__(self, message, code=None, response=None):
-        super().__init__(message)
-        self.code = code
-        self.response = response
+    """Raised when Bale API returns an error."""
+    pass
 
 
-class TimeoutError(HidMartError):
-    """Raised when an API request times out."""
+class NetworkError(HidMartError):
+    """Raised when a network request fails."""
+    pass
+
+
+class InvalidTokenError(HidMartError):
+    """Raised when the bot token is invalid."""
+    pass
