db6c67379ec4ae2ddc6c7645b1a7054ab4e72871
؟MARTO <programmers.atlantis@gmail.com>
2026-08-31 16:30:53 +0330

    Update __init__.py

--- hidmart/__init__.py
+++ hidmart/__init__.py
@@ -1,11 +1,5 @@
 from .bot import Bot
-
-from .types import (
-    Message,
-    User,
-    Chat,
-)
-
+from .types import Message, User, Chat
 from .exceptions import (
     HidMartError,
     APIError,
@@ -13,9 +7,7 @@ from .exceptions import (
     InvalidTokenError,
 )
 
-
-__version__ = "0.2.0"
-
+__version__ = "0.3.0"
 
 __all__ = [
     "Bot",
