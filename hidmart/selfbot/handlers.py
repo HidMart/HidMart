ca3135974b69b5d9979eaf385c2a03cffbb43e1d
؟MARTO <programmers.atlantis@gmail.com>
2026-09-01 17:25:34 +0330

    Update handlers.py

--- hidmart/selfbot/handlers.py
+++ hidmart/selfbot/handlers.py
@@ -1,56 +1,17 @@
-class Handler:
-
-    def __init__(
-        self,
-        callback,
-        command=None,
-        text=None
-    ):
-        self.callback = callback
-        self.command = command
-        self.text = text
+import inspect
 
 
 class HandlerManager:
-
     def __init__(self):
-        self.handlers = []
+        self._message_handlers = []
 
-    def add(
-        self,
-        callback,
-        command=None,
-        text=None
-    ):
-        handler = Handler(
-            callback,
-            command=command,
-            text=text
-        )
-
-        self.handlers.append(handler)
-
+    def add_message_handler(self, callback):
+        self._message_handlers.append(callback)
         return callback
 
-    async def dispatch(self, message):
+    async def dispatch_message(self, message):
+        for callback in self._message_handlers:
+            result = callback(message)
 
-        for handler in self.handlers:
-
-            if handler.command is not None:
-
-                text = message.text or ""
-
-                if not text.startswith("/"):
-                    continue
-
-                command = text[1:].split()[0]
-
-                if command != handler.command:
-                    continue
-
-            if handler.text is not None:
-
-                if message.text != handler.text:
-                    continue
-
-            await handler.callback(message)
+            if inspect.isawaitable(result):
+                await result
