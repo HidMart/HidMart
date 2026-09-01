fc7c45edfd1b00b8cc0eb83facf27d64d909c64f
؟MARTO <programmers.atlantis@gmail.com>
2026-09-01 18:07:35 +0330

    Update __init__.py

--- hidmart/__init__.py
+++ hidmart/__init__.py
@@ -1,12 +1,3 @@
-"""
-HidMart
-=======
-
-Async Python framework for building Bale Messenger bots.
-
-Version: 0.4.0
-"""
-
 from .bot import Bot
 from .types import Message, User, Chat
 from .exceptions import (
@@ -16,58 +7,33 @@ from .exceptions import (
     InvalidTokenError,
 )
 
-# SelfBot API
-from .selfbot import (
-    SelfBot,
-    SelfBotMessage,
-    SelfBotUser,
-    SelfBotChat,
-    Auth,
-    AuthState,
-    NewMessage,
-    SelfBotError,
-    SelfBotConnectionError,
-    SelfBotAuthenticationError,
-    SelfBotNotConnectedError,
+from .ui import (
+    InlineButton,
+    InlineKeyboard,
+    KeyboardButton,
+    ReplyKeyboard,
+    RemoveKeyboard,
 )
 
+
 __version__ = "0.4.0"
-__author__ = "HidMart"
-__license__ = "MIT"
+
 
 __all__ = [
-    # Main Bot
     "Bot",
 
-    # Bot Types
     "Message",
     "User",
     "Chat",
 
-    # Bot Exceptions
     "HidMartError",
     "APIError",
     "NetworkError",
     "InvalidTokenError",
 
-    # SelfBot
-    "SelfBot",
-
-    # SelfBot Models
-    "SelfBotMessage",
-    "SelfBotUser",
-    "SelfBotChat",
-
-    # SelfBot Auth
-    "Auth",
-    "AuthState",
-
-    # SelfBot Events
-    "NewMessage",
-
-    # SelfBot Exceptions
-    "SelfBotError",
-    "SelfBotConnectionError",
-    "SelfBotAuthenticationError",
-    "SelfBotNotConnectedError",
+    "InlineButton",
+    "InlineKeyboard",
+    "KeyboardButton",
+    "ReplyKeyboard",
+    "RemoveKeyboard",
 ]
