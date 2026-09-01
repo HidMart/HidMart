02800355deff5bfa47737d67ad078a60fc2f96d7
؟MARTO <programmers.atlantis@gmail.com>
2026-09-01 16:51:37 +0330

    Update __init__.py

--- hidmart/__init__.py
+++ hidmart/__init__.py
@@ -1,5 +1,6 @@
 from .bot import Bot
 from .types import Message, User, Chat
+
 from .exceptions import (
     HidMartError,
     APIError,
@@ -7,15 +8,51 @@ from .exceptions import (
     InvalidTokenError,
 )
 
-__version__ = "0.3.0"
+# SelfBot
+from .selfbot import (
+    SelfBot,
+    SelfBotMessage,
+    SelfBotUser,
+    SelfBotChat,
+)
+
+from .selfbot.exceptions import (
+    SelfBotError,
+    SelfBotConnectionError,
+    SelfBotAuthenticationError,
+    SelfBotAPIError,
+    SelfBotNotImplementedError,
+)
+
+
+__version__ = "0.4.0"
+
 
 __all__ = [
+    # Bot
     "Bot",
+
+    # Bot Types
     "Message",
     "User",
     "Chat",
+
+    # Bot Exceptions
     "HidMartError",
     "APIError",
     "NetworkError",
     "InvalidTokenError",
+
+    # SelfBot
+    "SelfBot",
+    "SelfBotMessage",
+    "SelfBotUser",
+    "SelfBotChat",
+
+    # SelfBot Exceptions
+    "SelfBotError",
+    "SelfBotConnectionError",
+    "SelfBotAuthenticationError",
+    "SelfBotAPIError",
+    "SelfBotNotImplementedError",
 ]
