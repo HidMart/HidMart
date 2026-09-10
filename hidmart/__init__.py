dcac7b3d5ffedab3562b1111b08ad67e38e1a7af
؟MARTO <programmers.atlantis@gmail.com>
2026-09-11 03:24:27 +0330

    Update __init__.py

--- hidmart/__init__.py
+++ hidmart/__init__.py
@@ -1,25 +1,17 @@
-__version__ = "0.1.0"
-
-from .selfbot import SelfBot
-from .selfbot.models import User, Chat, Message, Update
-from .selfbot.errors import (
-    HidMartError,
-    AuthenticationError,
-    ConnectionError,
-    ProtocolError,
-    SessionError,
+from .selfbot import (
+    SelfBot,
+    User,
+    Chat,
+    Message,
+    Update,
 )
 
+__version__ = "0.1.0"
+
 __all__ = [
-    "__version__",
     "SelfBot",
     "User",
     "Chat",
     "Message",
     "Update",
-    "HidMartError",
-    "AuthenticationError",
-    "ConnectionError",
-    "ProtocolError",
-    "SessionError",
 ]
